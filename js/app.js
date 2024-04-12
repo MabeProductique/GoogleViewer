665422ec50cccc04a49feee2c07d00f144b7c509
Martin Bérubé <martin.berube@productique.quebec>
2024-04-12 17:07:37 -0400

    add variant option

--- js/app.js
+++ js/app.js
@@ -1,33 +1,55 @@
 
 //Models
-const modelsChoice = ['Escalier1','Escalier2','Canoe', 'Chair', 'GeoPlanter', 'Mixer', 'NeilArmstrong', 'ToyTrain']
+const modelChoices = ['Shoe', 'Canoe', 'Chair', 'GeoPlanter', 'Mixer', 'NeilArmstrong', 'ToyTrain', 'Escalier1', 'Escalier2']
+
+//Options
+const modelNames = document.querySelector('#modelNames');
+const variantNames = document.querySelector('#variantNames');
+const modelViewer = document.querySelector("#modelViewer");
 
 //Load User Models Choice
-const modelsOption = document.getElementById('models');
-modelsOption.innerHTML = ''
-
-modelsChoice.forEach(model => {
+modelChoices.forEach(model => {
   const option = document.createElement('option');
   option.value = model;
   option.innerText = model;
-  modelsOption.appendChild(option);
+  modelNames.appendChild(option);
 });
 
 //First Model to show
-const modelViewer = document.getElementById('modelViewer')
 modelViewer.style.height = window.innerHeight + 'px'
 modelViewer.style.width = 'auto'
 modelViewer.style.margin = 'auto'
-modelViewer.setAttribute('src', 'assets/' + modelsChoice[0] + '.glb')
-modelViewer.setAttribute('poster', 'assets/' + modelsChoice[0] + '.webp')
+modelViewer.setAttribute('src', 'assets/' + modelChoices[0] + '.glb')
+modelViewer.setAttribute('poster', 'assets/' + modelChoices[0] + '.webp')
 
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Add event listener to change model on selection change
-  modelsOption.addEventListener('change', function () {
-    var selectedFileName = this.value
-    modelViewer.setAttribute('src', 'assets/' + selectedFileName + '.glb')
-    modelViewer.setAttribute('poster', 'assets/' + selectedFileName + '.webp')
-    modelViewer.setAttribute('alt', selectedFileName)
-  })
+modelNames.addEventListener('change', function () {
+  var selectedFileName = this.value
+  console.log('Select ' + selectedFileName)
+  modelViewer.setAttribute('src', 'assets/' + selectedFileName + '.glb')
+  modelViewer.setAttribute('poster', 'assets/' + selectedFileName + '.webp')
+  modelViewer.setAttribute('alt', selectedFileName)
 })
+
+modelViewer.addEventListener('load', (event) => {
+  console.log('Model loading')
+  //remove variants
+  Array.from(variantNames.children).forEach(child => child.remove());
+
+  const names = modelViewer.availableVariants;
+  for (const name of names) {
+    const option = document.createElement('option');
+    option.value = name;
+    option.textContent = name;
+    variantNames.appendChild(option);
+  }
+  // Adds a default option.
+  const option = document.createElement('option');
+    option.value = 'default';
+    option.textContent = 'Default';
+    variantNames.appendChild(option);
+});
+
+
+variantNames.addEventListener('change', (event) => {
+  modelViewer.variantName = event.target.value === 'default' ? null : event.target.value;
+});
